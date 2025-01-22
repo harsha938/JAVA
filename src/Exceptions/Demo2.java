60d62ff9907237457185d9771f5385eae0dc7536
Harsha Vardhan <bhaskaraharshavardhan123@gmail.com>
2025-01-22 10:27:48 +0530

    Throws

---
+++ src/Exceptions/Demo2.java
@@ -0,0 +1,22 @@
+package Exceptions;
+
+//Throws keyword is used to declare an exception. It gives an information to the programmer that there may occur an exception so it is better for the programmer to provide the exception handling code so that normal flow can be maintained.
+class A {
+
+    public void show() throws ClassNotFoundException {
+        Class.forName("Test");
+    }
+}
+
+public class Demo2 {
+
+//If throws used in main then it will ask the JVM to handle the exception.
+    public static void main(String[] args) {
+        A obj = new A();
+        try {
+          obj.show();
+        } catch (ClassNotFoundException e) {
+          e.printStackTrace();
+        }
+    }
+}
