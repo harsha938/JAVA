c297fcc14b8678f4e751f9d74f0b23d51b0bb7b0
Harsha Vardhan <bhaskaraharshavardhan123@gmail.com>
2025-01-23 11:27:46 +0530

    Sets

--- src/Collections/Sets/Demo.java
+++ src/Collections/Sets/Demo.java
@@ -1,25 +1,32 @@
 package Collections.Sets;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.TreeSet;
 
 public class Demo {
-  public static void main(String[] args) {
-    Set<Integer> set = new HashSet<Integer>();
-    set.add(10);
-    set.add(20);
-    set.add(30);
-    set.add(10);
-    set.add(20);
 
-    set.remove(20);
-    System.out.println(set.contains(20));
+    public static void main(String[] args) {
+        Collection<Integer> set = new TreeSet<>();
+        set.add(10);
+        set.add(20);
+        set.add(30);
+        set.add(10);
+        set.add(20);
 
-    System.out.println(set.isEmpty());
+        set.remove(20);
+        System.out.println(set.contains(20));
 
-    System.out.println(set.size());
-    for(int n:set){
-      System.out.println(n);
+        System.out.println(set.isEmpty());
+
+        System.out.println(set.size());
+
+        Iterator<Integer> it = set.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+        // for(int n:set){
+        //   System.out.println(n);
+        // }
     }
-  }
 }
